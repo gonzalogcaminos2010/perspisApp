ff55c5453277ccfb5598c320bf623abba2275ab5
Gonzalo Caminos <gonzalogabrielcaminos@gmail.com>
2022-02-18 16:50:54 -0300

    Tercer commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,6 +3,9 @@ import React from 'react'
 import { KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native-web'
 
 const LoginScreen = () => {
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
   return (
     <KeyboardAvoidingView
     style={styles.container}
@@ -10,14 +13,14 @@ const LoginScreen = () => {
         <View style={styles.inputContainer}>
             <TextInput
             placeholder="Username"
-            //value={}
-            //onChangeText={text =>}
+            value={email}
+            onChangeText={text => setEmail(text)}
             style={styles.input}
             />
             <TextInput
             placeholder="Password"
-            //value={}
-            //onChangeText={text =>}
+            value={password}
+            onChangeText={text => setPassword(text)}
             style={styles.input}
             secureTextEntry={true}
             />
@@ -74,9 +77,35 @@ const styles = StyleSheet.create({
         borderRadius:10,
         marginTop:5,
     },
-    buttonContainer: {},
-    button: {},
-    buttonText: {},
-    buttonOutline:{},
-    buttonOutlineText: {}
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:40,
+    },
+    button: {
+        backgroundColor:'#0782F9',
+        width:'100%',
+        padding:15,
+        borderRadius:10,
+        marginTop:5,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color:'white',
+        fontWeight:'700',
+        fontSize:16,
+
+    },
+    buttonOutline:{
+        backgroundColor:'white',
+        marginTop:5,
+        borderColor:'#0782F9',
+        borderWidth:2,
+    },
+    buttonOutlineText: {
+        color:'#0782F9',
+        fontWeight:'700',
+        fontSize:16,
+    }
 })
